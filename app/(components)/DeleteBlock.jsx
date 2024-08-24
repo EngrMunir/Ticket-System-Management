9d0b87fc5d8e5acdcdcd9280db979a09bee2a6ef
Munir <smsirajulmonir@gmail.com>
2024-08-24 10:35:37 -0700

    deleted block

---
+++ app/(components)/DeleteBlock.jsx
@@ -0,0 +1,9 @@
+import { FaFax } from "react-icons/fa6";
+
+const DeleteBlock = () => {
+    return (
+        <FaFax className="text-red-400 hover:cursor-pointer hover:text-red-200"/>
+    );
+};
+
+export default DeleteBlock;
