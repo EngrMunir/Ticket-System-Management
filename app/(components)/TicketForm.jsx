e02fc2b682e2aa041f84b9e0ba6925aa249d4012
Munir <smsirajulmonir@gmail.com>
2024-08-25 10:59:34 -0700

    connected with db

---
+++ app/(components)/TicketForm.jsx
@@ -0,0 +1,11 @@
+"use client"
+import { useRouter } from "next/navigation";
+import React, {useState} from "react";
+
+const TicketForm = () => {
+    return (
+        <div>TicketForm </div>
+    );
+};
+
+export default TicketForm;
